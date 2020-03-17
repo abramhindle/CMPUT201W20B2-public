b66a429fe991dc828120918532274128ed9134b3
Abram Hindle <abram.hindle@softwareprocess.es>
2020-03-17 11:21:48 -0600

    More script outputs

--- week10/build-coolbears.sh
+++ week10/build-coolbears.sh
@@ -6,3 +6,12 @@ gcc  -std=c99 -pedantic -Wall -Wextra -ftrapv -ggdb3 \
        -o coolbears-driver coolbears-driver.c \
        coolbears.o 
 ./coolbears-driver
+
+# build coolbears.o
+gcc  -std=c99 -pedantic -Wall -Wextra -ftrapv -ggdb3 \
+       -c coolbears.c
+# build coolbears-driver and link it to coolbears.o
+gcc  -std=c99 -pedantic -Wall -Wextra -ftrapv -ggdb3 \
+       -o coolbears-driver coolbears-driver.c \
+       coolbears.o 
+./coolbears-driver
